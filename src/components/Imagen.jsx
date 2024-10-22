2b90fba4f778594171113d046a763839fcc767bd
Vinicius <161778225+Viniciuss1789@users.noreply.github.com>
2024-10-21 21:08:53 -0300

    Add files via upload

---
+++ src/components/Imagen.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+    return(
+        <>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfm9jU2oy2pGYqPdNwFp9PbViRNT7v5IymXg&s" alt="youtube" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLO3w4IjPqI_YUZOuNHD-T73vUqvuOehSPrQ&s" alt="" />
+        </>
+    )
+}
+
+export default App
